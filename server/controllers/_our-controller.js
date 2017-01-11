07c715d60d431c632f3ead8ff7dd099ede9b5522
greengie <green.gie1234@gmail.com>
2017-01-11 18:20:59 +0700

    config new controller -- get data

--- server/controllers/_our-controller.js
+++ server/controllers/_our-controller.js
@@ -7,10 +7,11 @@ const pgp = require('pg-promise')();
 const db = require('../config/db');
 const Subjects = pgp(db.urlSubjectViews);
 
-exports.gdp = function(req, res, next) {
-  // console.log(req.params.year)
+exports.data = function(req, res, next) {
+  // console.log(req.params.selector)
   var year = req.params.year;
-  Subjects.any("select iso_alpha3 as country_code, country_list.country_name, year, value as gdp from gdp, country_list where (gdp.country_name = country_list.country_name) and (year = $1)", year)
+  var selector = req.params.selector;
+  Subjects.any("select iso_alpha3 as country_code, country_list.country_name, year, value as data_value from $2~, country_list where ($2~.country_name = country_list.country_name) and (year = $1)", [year, selector])
     .then(function (result) {
       console.log(result);
       res.status(200).json(result);
@@ -21,15 +22,15 @@ exports.gdp = function(req, res, next) {
     });
 }
 
-exports.popdensity = function(req, res, next) {
-  var year = req.params.year;
-  Subjects.any("select iso_alpha3 as country_code, country_list.country_name, year, value as popdensity from popdensity, country_list where (popdensity.country_name = country_list.country_name) and (year = $1)", year)
-    .then(function (result) {
-      console.log(result);
-      res.status(200).json(result);
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.status(404).json(error);
-    });
-}
+// exports.popdensity = function(req, res, next) {
+//   var year = req.params.year;
+//   Subjects.any("select iso_alpha3 as country_code, country_list.country_name, year, value as popdensity from popdensity, country_list where (popdensity.country_name = country_list.country_name) and (year = $1)", year)
+//     .then(function (result) {
+//       console.log(result);
+//       res.status(200).json(result);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//       res.status(404).json(error);
+//     });
+// }
