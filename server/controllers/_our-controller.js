f791b31ab56a907bc1c0a58b1de2cb9982e0fa1f
greengie <green.gie1234@gmail.com>
2017-01-20 17:49:22 +0700

    new version [no null value query]

--- server/controllers/_our-controller.js
+++ server/controllers/_our-controller.js
@@ -11,7 +11,7 @@ exports.data = function(req, res, next) {
   // console.log(req.params.selector)
   var year = req.params.year;
   var selector = req.params.selector;
-  Subjects.any("select iso_alpha3 as country_code, country_list.country_name, year, value as data_value from $2~, country_list where ($2~.country_name = country_list.country_name) and (year = $1)", [year, selector])
+  Subjects.any("select iso_alpha3 as country_code, country_list.country_name, year, value as data_value from $2~, country_list where ($2~.country_name = country_list.country_name) and (year = $1) and (value is not null)", [year, selector])
     .then(function (result) {
       console.log(result);
       res.status(200).json(result);
