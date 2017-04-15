a8a5921eabfb00de3f0d92f78cfba5dc6a43e237
greengie <green.gie1234@gmail.com>
2017-04-15 16:49:56 +0700

    add map api

--- server/router.js
+++ server/router.js
@@ -21,11 +21,14 @@ module.exports = function(app) {
   apiRoutes.get('/data_migration/:selector/:year', _ourController.data_migration);
   apiRoutes.get('/correlation/:selectorX/:selectorY/:yearMin/:yearMax/:scale_x/:scale_y/:query_type', _ourController.correlation);
   apiRoutes.get('/prediction/:userid/:fileid', _ourController.prediction);
+  apiRoutes.get('/country', _ourController.country);
   apiRoutes.post('/user', _ourController.checkUserAreInDB);
   apiRoutes.get('/limit/:id', _ourController.getFileLimit);
   apiRoutes.get('/list/:id', _ourController.getFileList);
   apiRoutes.post('/upload', _ourController.uploadCSV);
   apiRoutes.post('/calregression/:label_x/:label_y', _ourController.calculateRegression);
+  apiRoutes.get('/jsonworld', _ourController.world);
+  apiRoutes.get('/migration_map/:year', _ourController.migration_map);
   //tell the app to use apiRoutes
   //and set the base url to
   // localhost:3000/api
