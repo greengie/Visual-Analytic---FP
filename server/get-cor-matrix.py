6a4636970ac6649b8a264fdb28bb10ea6a877f2a
greengie <green.gie1234@gmail.com>
2017-04-23 13:47:01 +0700

    fix get-cor-matrix.py remove unused columns

--- server/get-cor-matrix.py
+++ server/get-cor-matrix.py
@@ -9,9 +9,8 @@ file_name = sys.argv[3]
 
 open_path = file_path+folder_id+'/file/'+file_name
 df1=pd.read_csv(open_path)
-rho = df1.corr()
+new_df = df1.drop('year', 1)
+new_df = new_df.drop('country_name', 1)
+new_df.to_csv(open_path, index = False)
+rho = new_df.corr()
 rho.to_csv(file_path+folder_id+'/cor-data/cor-'+file_name)
-# with open(file_name_1, 'rb') as csvfile1:
-# 	filereader1 = csv.DictReader(csvfile1)
-# 	for x in filereader1:
-# 		print x
